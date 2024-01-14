813923cc4b89990a56933b604baee7ddc2cd7707
Aaron Hoffman <kd0sgh@gmail.com>
2024-01-14 15:03:34 -0600

    Add keybindings when cmake-tools is active

--- plugins/community.lua
+++ plugins/community.lua
@@ -7,7 +7,31 @@ return {
   { import = "astrocommunity.colorscheme.sonokai" },
 
   { import = "astrocommunity.pack.rust" },
+
   { import = "astrocommunity.pack.cpp" },
+  -- Add some configuration when cmake-tools is active.
+  {
+    "Civitasv/cmake-tools.nvim",
+    keys = {
+      { "<leader>mg", "<cmd>CMakeGenerate<cr>", desc = "Generate" },
+      { "<leader>mb", "<cmd>CMakeBuild<cr>", desc = "Build" },
+      { "<leader>mc", "<cmd>CMakeClean<cr>", desc = "Clean" },
+      { "<leader>mp", "<cmd>CMakeSelectConfigurePreset<cr>", desc = "Select configure preset" },
+      { "<leader>mP", "<cmd>CMakeSelectBuildPreset<cr>", desc = "Select build preset" },
+      { "<leader>mt", "<cmd>CMakeShowTargetFiles<cr>", desc = "Show target files" },
+    },
+    config = function()
+      local prefix = "<leader>m"
+      local maps = { n = {}, x = {} }
+
+      local icon = vim.g.icons_enabled and "󰛔 " or ""
+      maps.n[prefix] = { desc = icon .. "CMake" }
+      maps.x[prefix] = { desc = icon .. "CMake" }
+
+      require("astronvim.utils").set_mappings(maps)
+    end,
+  },
+
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.docker" },
   { import = "astrocommunity.pack.markdown" },
