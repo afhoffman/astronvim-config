eef935f60cffd4208d25c916bb81841729155ce7
Aaron Hoffman <kd0sgh@gmail.com>
2024-01-23 08:21:15 -0600

    Revert "Add nvim spider"
    This reverts commit 6869abc71b998d1fbc7987ad057c100489f7904f.

--- plugins/community.lua
+++ plugins/community.lua
@@ -49,7 +49,6 @@ return {
   { import = "astrocommunity.pack.go" },
 
   { import = "astrocommunity.motion.hop-nvim" },
-  { import = "astrocommunity.motion.nvim-spider" },
 
   { import = "astrocommunity.markdown-and-latex.vimtex" },
 }
